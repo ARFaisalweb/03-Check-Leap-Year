3fa3aa9c1ff904236b495e012a9ba8c2b835f8c9
coderFaiisal <officialfaisal1112@gmail.com>
2024-09-17 13:29:18 +0600

    test changes

--- LeapYear.js
+++ LeapYear.js
@@ -1,10 +1,10 @@
-function checkLeapYear(year){
-    if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
-       return 'Leap year';
-    }
-    else{
-        return 'Not leap year';
-    }
+function checkLeapYear(year) {
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    return "Leap year";
+  } else {
+    return "Not leap year";
+  }
+  console.log("Test")
 }
 const result = checkLeapYear(2000);
 console.log(result);
